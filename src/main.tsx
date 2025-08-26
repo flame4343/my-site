7af65a539dde18cd7a3d6658df83b61144b717cc
fengzebang8336 <fengzebang8336@navinfo.com>
2025-08-26 10:29:10 +0800

    fix(router): add basename for GitHub Pages subpath

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
+// src/main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-site">
       <App />
       <Toaster />
     </BrowserRouter>
